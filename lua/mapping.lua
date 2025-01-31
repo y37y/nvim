fe9acca581e3edc26c5d3aa9a791a0a859a92469
y37y <y37y@y37y>
2025-01-31 15:09:33 -0800

    feat: update mappings with upstream changes

--- lua/mapping.lua
+++ lua/mapping.lua
@@ -6,44 +6,44 @@ function M.core_mappings(mappings)
   local maps = mappings
   if maps then
     maps.n["<Leader>n"] = false
-
+    maps.n["<Leader>s"] = { desc = require("astroui").get_icon("GrugFar", 1, true) .. "Search" }
+    maps.v["<Leader>s"] = { desc = require("astroui").get_icon("GrugFar", 1, true) .. "Search" }
+    
     maps.n.n = { require("utils").better_search "n", desc = "Next search" }
     maps.n.N = { require("utils").better_search "N", desc = "Previous search" }
 
     maps.v["K"] = { ":move '<-2<CR>gv-gv", desc = "Move line up", silent = true }
     maps.v["J"] = { ":move '>+1<CR>gv-gv", desc = "Move line down", silent = true }
 
-    -- Add these clipboard mappings
+    -- Keep your clipboard mappings
     maps.v["<leader>y"] = { '"+y', desc = "Copy selected text to system clipboard" }
     maps.v["<leader>Y"] = { '"+Y', desc = "Copy selected line to system clipboard" }
     maps.n["<leader>y"] = { '"+y', desc = "Yank to system clipboard" }
     maps.n["<leader>Y"] = { '"+Y', desc = "Yank line to system clipboard" }
-    -- Make regular y copy to system clipboard in both modes
     maps.n["y"] = { '"+y', desc = "Copy to system clipboard" }
     maps.v["y"] = { '"+y', desc = "Copy to system clipboard" }
-    -- Copy entire buffer to system clipboard
     maps.n["<leader>ya"] = { ':%y+<CR>', desc = "Yank entire buffer to system clipboard" }
-    -- Select from true beginning to true end (g_ goes to last non-blank character)
+
+    -- Keep your buffer select mapping
     maps.n["<leader>a"] = { 
-    function()
-        vim.cmd('normal! gg0')
-        vim.cmd('normal! vG$')
-    end,
-    desc = "Select entire buffer from start to end" 
-    }
-    -- vertical diff with clipboard
-    maps.n["<leader>dv"] = { 
-    function()
-      -- Create new vertical split
-      vim.cmd('vnew')
-      -- Paste clipboard content into the new buffer
-      vim.cmd('normal! "+P')
-      -- Set both buffers to diff mode
-      vim.cmd('windo diffthis')
-    end,
-    desc = "Vertical diff split with clipboard" 
+      function()
+          vim.cmd('normal! gg0')
+          vim.cmd('normal! vG$')
+      end,
+      desc = "Select entire buffer from start to end" 
     }
 
+    -- Keep your vertical diff mapping
+    maps.n["<leader>dv"] = { 
+      function()
+        vim.cmd('vnew')
+        vim.cmd('normal! "+P')
+        vim.cmd('windo diffthis')
+      end,
+      desc = "Vertical diff split with clipboard" 
+    }
+
+    -- Keep your macOS mappings
     if system == "Darwin" then
       maps.i["<D-s>"] = { "<esc>:w<cr>a", desc = "Save file", silent = true }
       maps.x["<D-s>"] = { "<esc>:w<cr>a", desc = "Save file", silent = true }
@@ -53,22 +53,14 @@ function M.core_mappings(mappings)
     maps.n["n"] = { "nzz" }
     maps.n["N"] = { "Nzz" }
 
-    -- close search highlight
     maps.n["<Leader>nh"] = { ":nohlsearch<CR>", desc = "Close search highlight", silent = true }
-
     maps.n["H"] = { "^", desc = "Go to start without blank" }
     maps.n["L"] = { "$", desc = "Go to end without blank" }
-
     maps.v["<"] = { "<gv", desc = "Unindent line" }
     maps.v[">"] = { ">gv", desc = "Indent line" }
     maps.t["<Esc>"] = { [[<C-\><C-n>]], desc = "Exit terminal mode" }
-
-    -- 在visual mode 里粘贴不要复制
     maps.n["x"] = { '"_x', desc = "Cut without copy" }
 
-    maps.n["<Leader>bo"] = maps.n["<Leader>bc"]
-
-    -- lsp restart
     maps.n["<Leader>lm"] = { "<Cmd>LspRestart<CR>", desc = "Lsp restart" }
     maps.n["<Leader>lg"] = { "<Cmd>LspLog<CR>", desc = "Show lsp log" }
 
@@ -93,9 +85,8 @@ function M.core_mappings(mappings)
       }
     end
 
-    -- window
     local get_icon = require("astroui").get_icon
-    maps.n["<Leader>w"] = { desc = get_icon("Window", 1, true) }
+    maps.n["<Leader>w"] = { name = get_icon("Window", 1, true) .. "Window" }
     maps.n["<Leader>wc"] = { "<C-w>c", desc = "Close current screen" }
     maps.n["<Leader>wo"] = { "<C-w>o", desc = "Close other screen" }
     maps.n["<Leader>we"] = { "<C-w>=", desc = "Equals All Window" }
