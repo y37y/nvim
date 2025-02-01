d5abb019561bded2a0f69a864fdaf663a1abb633
y37y <y37y@y37y>
2025-01-31 16:32:17 -0800

    update mapping

--- lua/mapping.lua
+++ lua/mapping.lua
@@ -14,8 +14,10 @@ function M.core_mappings(mappings)
     maps.n.N = { require("utils").better_search "N", desc = "Previous search" }
     maps.v["K"] = { ":move '<-2<CR>gv-gv", desc = "Move line up", silent = true }
     maps.v["J"] = { ":move '>+1<CR>gv-gv", desc = "Move line down", silent = true }
+
     maps.n["n"] = { "nzz" }
     maps.n["N"] = { "Nzz" }
+
     -- close search highlight
     maps.n["<Leader>nh"] = { ":nohlsearch<CR>", desc = "Close search highlight", silent = true }
 
@@ -33,29 +35,30 @@ function M.core_mappings(mappings)
     maps.n["<leader>Y"] = { '"+Y', desc = "Yank line to system clipboard" }
     maps.n["y"] = { '"+y', desc = "Copy to system clipboard" }
     maps.v["y"] = { '"+y', desc = "Copy to system clipboard" }
-    maps.n["<leader>ya"] = { ':%y+<CR>', desc = "Yank entire buffer to system clipboard" }
+    maps.n["<leader>ya"] = { ":%y+<CR>", desc = "Yank entire buffer to system clipboard" }
 
     -- Buffer select mapping
     maps.n["<leader>a"] = {
       function()
-          vim.cmd('normal! gg0')
-          vim.cmd('normal! vG$')
+        vim.cmd "normal! gg0"
+        vim.cmd "normal! vG$"
       end,
-      desc = "Select entire buffer from start to end"
+      desc = "Select entire buffer from start to end",
     }
 
     -- Vertical diff mapping
     maps.n["<leader>dv"] = {
       function()
-        vim.cmd('vnew')
-        vim.cmd('normal! "+P')
-        vim.cmd('windo diffthis')
+        vim.cmd "vnew"
+        vim.cmd 'normal! "+P'
+        vim.cmd "windo diffthis"
       end,
-      desc = "Vertical diff split with clipboard"
+      desc = "Vertical diff split with clipboard",
     }
 
     -- Cut without copy in visual mode
     maps.n["x"] = { '"_x', desc = "Cut without copy" }
+
     -- lsp restart
     maps.n["<Leader>lm"] = { "<Cmd>LspRestart<CR>", desc = "Lsp restart" }
     maps.n["<Leader>lg"] = { "<Cmd>LspLog<CR>", desc = "Show lsp log" }
@@ -68,30 +71,44 @@ function M.core_mappings(mappings)
     end
 
     -- Enable Ctrl+hjkl navigation in debug/test mode
-    for _, mode in ipairs({ "n", "i", "v" }) do
+    for _, mode in ipairs { "n", "i", "v" } do
       maps[mode]["<C-h>"] = {
-        function()
-          require("astrocore").move_selection "h"
-        end,
+        function() require("astrocore").move_selection "h" end,
         desc = "Move to left screen",
       }
       maps[mode]["<C-j>"] = {
-        function()
-          require("astrocore").move_selection "j"
-        end,
+        function() require("astrocore").move_selection "j" end,
         desc = "Move to bottom screen",
       }
       maps[mode]["<C-k>"] = {
-        function()
-          require("astrocore").move_selection "k"
-        end,
+        function() require("astrocore").move_selection "k" end,
         desc = "Move to top screen",
       }
       maps[mode]["<C-l>"] = {
-        function()
-          require("astrocore").move_selection "l"
-        end,
+        function() require("astrocore").move_selection "l" end,
         desc = "Move to right screen",
+      }
+    end
+
+    -- Lazy tools integration
+    if vim.fn.executable "lazygit" == 1 then
+      maps.n["<Leader>tl"] = {
+        function() require("utils").toggle_lazy_git() end,
+        desc = "ToggleTerm lazygit",
+      }
+    end
+
+    if vim.fn.executable "lazydocker" == 1 then
+      maps.n["<Leader>td"] = {
+        function() require("utils").toggle_lazy_docker() end,
+        desc = "ToggleTerm lazydocker",
+      }
+    end
+
+    if vim.fn.executable "btm" == 1 then
+      maps.n["<Leader>tt"] = {
+        function() require("utils").toggle_btm() end,
+        desc = "ToggleTerm btm",
       }
     end
 
@@ -99,6 +116,18 @@ function M.core_mappings(mappings)
     local get_icon = require("astroui").get_icon
     maps.n["<Leader>w"] = { name = get_icon("Window", 1, true) .. "Window" }
     maps.n["<Leader>wc"] = { "<C-w>c", desc = "Close current screen" }
+    maps.n["<Leader>wo"] = { "<C-w>o", desc = "Close other screen" }
+    maps.n["<Leader>we"] = { "<C-w>=", desc = "Equals All Window" }
+  end
+  return mappings
+end
+
+function M.lsp_mappings(mappings)
+  if not mappings then mappings = require("astrocore").empty_map_table() end
+  local maps = mappings
+  if maps then
+    maps.n["gK"] = false
+    maps.n["gk"] = maps.n["<Leader>lh"]
   end
   return mappings
 end
